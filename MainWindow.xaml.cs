ee54392709ef19ff49421e932beb4f9a7ee9df32
Stanislav-003 <140276613+Stanislav-003@users.noreply.github.com>
2024-10-17 23:38:36 +0300

    add pagination functional

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,65 +1,152 @@
 ﻿using System.Collections.ObjectModel;
-using System.Net.Http;
-using System.Text;
-using System.Text.Json;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Digital_Cloud_Technologies
+namespace Digital_Cloud_Technologies;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
-    public partial class MainWindow : Window
+    public ObservableCollection<CryptoCurrency> Members { get; set; } = new ObservableCollection<CryptoCurrency>();
+    private ObservableCollection<CryptoCurrency> _allMembers = new ObservableCollection<CryptoCurrency>();
+
+    public string ResponseContent
     {
-        public string ResponseContent
+        get { return (string)GetValue(ResponseContentProperty); }
+        set { SetValue(ResponseContentProperty, value); }
+    }
+
+    public static readonly DependencyProperty ResponseContentProperty =
+        DependencyProperty.Register("ResponseContent", typeof(string), typeof(MainWindow));
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+        MakeRequest();
+        SelectedRecord = "10";
+        UpdatePagedMembers();
+    }
+
+    private async void MakeRequest()
+    {
+        ResponseContent = "Загрузка...";
+
+        //await Task.Yield();
+
+        var cryptoResponse = await Utilities.FetchCryptoCoinInfo();
+
+        if (cryptoResponse != null)
         {
-            get { return (string)GetValue(ResponseContentProperty); }
-            set { SetValue(ResponseContentProperty, value); }
-        }
+            ResponseContent = Utilities.FormatCryptoResponse(cryptoResponse);
 
-        public static readonly DependencyProperty ResponseContentProperty =
-            DependencyProperty.Register("ResponseContent", typeof(string), typeof(MainWindow));
-
-        public ObservableCollection<CryptoCurrency> Members { get; set; } = new ObservableCollection<CryptoCurrency>();
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            MakeRequest();
-        }
-
-        private async void MakeRequest()
-        {
-            ResponseContent = "Загрузка...";
-
-            //await Task.Yield();
-
-            var cryptoResponse = await Utilities.FetchCryptoCoinInfo();
-
-            if (cryptoResponse != null)
+            _allMembers.Clear();
+            foreach (var m in cryptoResponse.Response)
             {
-                ResponseContent = Utilities.FormatCryptoResponse(cryptoResponse);
+                _allMembers.Add(m);
             }
-            else
-            {
-                ResponseContent = "Не удалось получить данные.";
-            }
-
-            Members.Clear();
-            foreach (var m in cryptoResponse!.Response)
-            {
-                Members.Add(m);
-            }
+            CurrentPage = 1;
+            UpdatePagination();
         }
-
-        private void btnRefreshRequest_Click(object sender, RoutedEventArgs e)
+        else
         {
-            MakeRequest();
+            ResponseContent = "Не удалось получить данные.";
         }
+    }
+
+    private void btnRefreshRequest_Click(object sender, RoutedEventArgs e)
+    {
+        MakeRequest();
+    }
+
+    private void UpdatePagination()
+    {
+        int pageSize = int.Parse(SelectedRecord);
+        NumberOfPages = (_allMembers.Count + pageSize - 1) / pageSize;
+        CurrentPage = Math.Min(CurrentPage, NumberOfPages);
+        UpdatePagedMembers();
+    }
+
+    private void UpdatePagedMembers()
+    {
+        int pageSize = int.Parse(SelectedRecord);
+        int startIndex = (CurrentPage - 1) * pageSize;
+        Members.Clear();
+        if (_allMembers.Any())
+        {
+            foreach (var member in _allMembers.Skip(startIndex).Take(pageSize))
+            {
+                Members.Add(member);
+            }
+        }
+    }
+
+    private int _currentPage = 1; 
+
+    public int CurrentPage
+    {
+        get { return _currentPage; }
+
+        set 
+        {
+            _currentPage = value;
+            OnPropertyChanged(nameof(CurrentPage));
+            UpdatePagedMembers();
+        }
+    }
+
+    private int _numberOfPages = 10;
+
+    public int NumberOfPages
+    {
+        get { return _numberOfPages; }
+
+        set
+        {
+            _numberOfPages = value;
+            OnPropertyChanged(nameof(NumberOfPages));
+        }
+    }
+
+    private string _selectedRecord = "10";
+
+    public string SelectedRecord
+    { 
+        get { return _selectedRecord; }
+        set 
+        {
+            _selectedRecord = value;
+            OnPropertyChanged(nameof(SelectedRecord));
+            UpdatePagination();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void btnFirstPage_Click(object sender, RoutedEventArgs e)
+    {
+        CurrentPage = 1;
+    }
+
+    private void btnPreviousPage_Click(object sender, RoutedEventArgs e)
+    {
+        if (CurrentPage > 1)
+            CurrentPage--;
+    }
+
+    private void btnNextPage_Click(object sender, RoutedEventArgs e)
+    {
+        if (CurrentPage < NumberOfPages)
+            CurrentPage++;
+    }
+
+    private void btnLastPage_Click(object sender, RoutedEventArgs e)
+    {
+        CurrentPage = NumberOfPages;
     }
 }
--- Views/CryptoMemberView.xaml.cs
+++ Views/CryptoMemberView.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace Digital_Cloud_Technologies.Views
+namespace Digital_Cloud_Technologies.Views;
+
+/// <summary>
+/// Interaction logic for CryptoMemberView.xaml
+/// </summary>
+public partial class CryptoMemberView : UserControl
 {
-    /// <summary>
-    /// Interaction logic for CryptoMemberView.xaml
-    /// </summary>
-    public partial class CryptoMemberView : UserControl
+    public CryptoMemberView()
     {
-        public CryptoMemberView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
